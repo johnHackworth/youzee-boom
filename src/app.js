89674a8bd7e01a846364de3789201c5129ad6a79
= <javieralvarezlop@youzee.com>
2012-10-06 19:40:54 +0200

    v2.0

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 var yz = {
+  currentZoom: 14,
   youzeers: {
     type: 'cartodb',
     user_name: 'xabel',
@@ -16,9 +17,10 @@ var yz = {
     "#{{table_name}}[name='Yunait'] {marker-fill:yellow}" +
     "#{{table_name}}[name='Funddy'] {marker-fill:#77F}" +
     "#{{table_name}}[name='Spartanbits'] {marker-fill:#FFDFDF}" +
-    "#{{table_name}}[name='Atlassian (Australia!)'] {marker-fill:#59F}" +
-    "#{{table_name}}[name='Fever (Barcelona)'] {marker-fill:#666}" +
+    "#{{table_name}}[name='Atlassian'] {marker-fill:#59F}" +
+    "#{{table_name}}[name='Fever'] {marker-fill:#222}" +
     "#{{table_name}}[name='Fjord'] {marker-fill:#FFF}" +
+    "#{{table_name}}[name='Luckia'] {marker-fill:red}" +
     "#{{table_name}}::labels[name!='Youzee']  { "+
     "text-name:[name]; "+
     "text-face-name:@font_reg;"+
@@ -64,12 +66,11 @@ var yz = {
     ]
   },
   mapConfig: {
-    title: "The Youzee Diaspora",
-    description: "Youzee engineering & design team's new jobplaces, after the crash of 2012 summer. If someone else already has found a new position and I'm not aware of it, please tell me, you know where to find me!  :)",
-    center: [40.421317, -3.705439],
+    center: [40.430501, -3.692404],
     zoom: 14,
     maxZoom: 20,
-    minZoom: 11,
+    minZoom: 2,
+    height: 400,
     zoomAnimation: true,
     overlays: [{
         type: 'header'
@@ -80,14 +81,35 @@ var yz = {
       }
     ]
   },
+  followText: function(name) {
+    var texts = [
+      "see "+name+" at twitter",
+      "say hello to "+name+" twitter",
+      name+" twitter",
+      "click to see "+name+" twitter",
+      "to the "+name+" twitter, and furthermore!",
+      name+ ", I'm your twitter",
+      "twit twit twitter!",
+      "click me to go to "+name+" twitter",
+      "click to twitter!",
+      "you're about to go to "+name+" twitter",
+      "follow this link to "+name+" twitter"
+    ]
+    var who = Math.floor(Math.random() * texts.length);
+    return texts[who];
+  },
   deployPeople: function() {
     var $peopleContainer = $('#people');
     for(var i=0, l=companies.length; i < l; i++) {
       for(var j=0, ll=companies[i].people.length; j < ll; j++) {
-        $peopleContainer.append('<div class="person" data-color="'+companies[i].color+'" data-position="'+companies[i].location+'"><img src="'+companies[i].people[j].image+'"></img><span>@'+companies[i].people[j].name+'</span></div>');
+        $peopleContainer.append('<a target="_blank" data-company="'+companies[i].company+'" href="http://twitter.com/'+companies[i].people[j].name+'" class="person" data-zoom="'+companies[i].zoom+'" data-color="'+companies[i].color+'" data-position="'+companies[i].location+'"><img src="'+companies[i].people[j].image+'"></img><span>@'+companies[i].people[j].name+'</span></a>');
       }
     }
-
+    $('a').tipsy({
+      fade: true,
+      gravity: 'w',
+      title: function() { return  yz.followText($(this).text())}//this.getAttribute('original-title').toUpperCase(); }
+    });
     setTimeout(this.perrify.bind(this), 150000*(1+Math.random()))
   },
   perrify: function() {
@@ -104,36 +126,106 @@ var yz = {
     $('#people .person').on('mouseover click', function(e) {
       $element = $(e.currentTarget);
       var position = $element.data('position').split(',');
+      var zoomLevel = $element.data('zoom');
+      var name = $element.data('company');
       yz.vis.map.setCenter(position.reverse());
+      yz.setZoom(yz.currentZoom, zoomLevel);
+      var company = yz.getCompany(name);
+      if(company.description) {
+        $('article.description').fadeIn();
+        $('article.description h3').html(name);
+        $('article.description span').html(company.description);
+      } else {
+        $('article.description').fadeOut();
+      }
+
+      // yz.bounceZoom(zoomLevel);
+
       $element.css('background-color', $element.data('color'));
     }).on('mouseout', function(e) {
       $element = $(e.currentTarget);
       $element.css('background-color', 'transparent');
     })
 
+    $('#people').on('mouseout', function(){
+        yz.mouseoutDate = new Date();
+        setTimeout(function() {
+          var currentDate = new Date();
+          if(currentDate - yz.mouseoutDate > 4000) {
+            $('article.description').fadeOut();
+          }
+        },5000);
+    });
+
+    $('#map').on('mouseout', function() {
+      $('.leaflet-tile-pane').click();
+    })
+  },
+  setZoom: function(currentLevel, destLevel) {
+    if (currentLevel === destLevel) {
+      return true;
+    } else {
+      yz.currentZoom = currentLevel;
+      vis.map.setZoom(currentLevel)
+      setTimeout(function() {
+        var nextLevel = 0;
+        if (currentLevel < destLevel) {
+          nextLevel = currentLevel + 1;
+        } else {
+          nextLevel = currentLevel - 1;
+        }
+        yz.setZoom(nextLevel, destLevel);
+      }, 200);
+    }
+  },
+  bounceZoom: function(destLevel) {
+    vis.map.setZoom(17);
+    setTimeout(function() {
+      yz.setZoom(17, destLevel);
+      // vis.map.setZoom(destLevel)
+    },2000)
+  },
+  getCompany: function(name) {
+    for(var i = 0, l = companies.length; i < l; i++) {
+      if(companies[i].company == name) return companies[i]
+    }
   }
 }
 
 var companies = [
-  {color: "yellow", location:[-3.692404, 40.428264], company: 'yunait', people: [{name: 'leidan', image: 'https://si0.twimg.com/profile_images/1131270006/Avatar2010_medium_reasonably_small.jpg'}]},
-  {color: "#FFDFDF", location:[-3.707097, 40.420501], company: "spartanbits", people:  [{name: 'pumpkin', image: 'https://si0.twimg.com/profile_images/1881877491/avatar_reasonably_small.jpg'}]},
-  {color: "#CF3", location:[-3.699185, 40.422424], company: "vizzuality", people:  [{name: 'johnhackworth', image: 'https://si0.twimg.com/profile_images/2460338539/814qnn0rrw7icdlcfdg7_reasonably_small.gif'}]},
-  {color: "#FFF", location:[-3.692951, 40.409117], company: "fjord", people:  [{name: 'jorgecorrea', image: 'https://si0.twimg.com/profile_images/2255209386/avatar_fjord_reasonably_small.png'}]},
-  {color: "#00F", location:[-3.696127, 40.415959], company: "tuenti", people:  [{name: 'ieduardogf', image: 'https://si0.twimg.com/profile_images/1614549841/image_reasonably_small.jpg'}]},
-  {color: "#77F", location:[-3.707843, 40.423307], company: "funddy", people:  [{name: 'amartinj', image: 'https://si0.twimg.com/profile_images/1332873130/alex-snow_twitter_reasonably_small.png'},{name: 'mrrocks', image: 'https://si0.twimg.com/profile_images/2327871935/xzzuabgzz5ivkzxve76l_reasonably_small.png'},{name: 'keyvanakbary', image: 'https://si0.twimg.com/profile_images/2216189429/43d9c6fa-53d2-4332-b533-0946a4bc62a7_reasonably_small.png'}]},
-  {color: "#59F", location:[-3.568497, 40.464189], company: "atlassian", people:  [{name: 'scinos', image: 'https://si0.twimg.com/profile_images/1543714613/cinos_reasonably_small.png'}]},
-  {color: "#666", location:[-3.568153, 40.472612], company: "fever", people:  [{name: 'sruiz', image: 'https://si0.twimg.com/profile_images/2235866660/SR_twitter.png'}]}
+  {description: "Yunait is a deal aggregator. It's the one stop place that groups the best deals and organizes them, giving the users the power to decide the kind of offers they will see depending on their tastes and interests.",
+    zoom: 14, color: "yellow", location:[-3.692404, 40.428264], company: 'yunait', people: [{name: 'leidan', image: 'https://si0.twimg.com/profile_images/1131270006/Avatar2010_medium_reasonably_small.jpg'}]},
+  {description: "Spartanbits is more than creating value through apps. It’s about creating an emotional bond with the user; it’s about delivering love to everyone, from our employees to the people that use our apps every day",
+    zoom: 17, color: "#FFDFDF", location:[-3.707097, 40.420501], company: "spartanbits", people:  [{name: 'pumpkin', image: 'https://si0.twimg.com/profile_images/1881877491/avatar_reasonably_small.jpg'}]},
+  {description:"Trying to make the world better since 2007! Vizzuality take enjoyment in being a small and productive company that delivers great user experiences for stories that matter. In this time they have worked with some of the biggest conservation organizations, NASA, Google, and others! And they have a <a href='http://pics.lockerz.com/s/250525221' target='_blank'>fucking</a> <a href='https://twitter.com/i/#!/demimismo/media/slideshow?url=http%3A%2F%2Finstagr.am%2Fp%2FQVBgCYQbEv%2F' target='_blank'>great</a> <a href='https://twitter.com/JohnHackworth/status/252747260302094336/photo/1' target='_blank'>office</a> :D",
+    zoom: 14, color: "#CF3", location:[-3.700429, 40.4346914], company: "vizzuality", people:  [{name: 'johnhackworth', image: 'https://si0.twimg.com/profile_images/2460338539/814qnn0rrw7icdlcfdg7_reasonably_small.gif'}]},
+  {description: "Fjord Spain opened in Madrid in late 2009 as a response to strong client demand and today we're already captivating leading brands like Telefonica and BBVA with our elegant, simple designs.",
+    zoom: 13, color: "#FFF", location:[-3.692951, 40.409117], company: "fjord", people:  [{name: 'jorgecorrea', image: 'https://si0.twimg.com/profile_images/2255209386/avatar_fjord_reasonably_small.png'}]},
+  {description: "Tuenti is a social communication platform for web and mobile, and there’s plenty to be said about our company",
+    zoom: 13, color: "#00F", location:[-3.696127, 40.415959], company: "tuenti", people:  [{name: 'ieduardogf', image: 'https://si0.twimg.com/profile_images/1614549841/image_reasonably_small.jpg'}]},
+  {description: "Funddy is a service that makes easy to collect money to achieve everything you want",
+    zoom: 17, color: "#77F", location:[-3.707843, 40.423307], company: "funddy", people:  [{name: 'amartinj', image: 'https://si0.twimg.com/profile_images/1332873130/alex-snow_twitter_reasonably_small.png'},{name: 'mrrocks', image: 'https://si0.twimg.com/profile_images/2327871935/xzzuabgzz5ivkzxve76l_reasonably_small.png'},{name: 'keyvanakbary', image: 'https://si0.twimg.com/profile_images/2216189429/43d9c6fa-53d2-4332-b533-0946a4bc62a7_reasonably_small.png'}]},
+  {description: "Software is everywhere – on our computers, our watches, our refrigerators. And the teams building software together – developers, product managers, doc writers, bug-bashers – are our heroes. Atlassian products help teams of all sizes track and share everything, work smarter, and create better software together.",
+    zoom: 11, color: "#59F", location:[151.2071, -33.8671], company: "atlassian", people:  [{name: 'scinos', image: 'https://si0.twimg.com/profile_images/1543714613/cinos_reasonably_small.png'}]},
+  {description: "Fever changes the way people make plans and meet new people you are going to like. Fever helps you to decide where to go at all times in a more personalized way. It is the essential tool for going out and finding",
+    zoom: 12, color: "#666", location:[2.156142, 41.395306], company: "fever", people:  [{name: 'sruiz', image: 'https://si0.twimg.com/profile_images/2235866660/SR_twitter.png'}]},
+  {
+    zoom: 12, color: "red", location: [-3.6890140, 40.399752], company: "luckia", people: [{"name": "torgus_2punto0", "image": "https://si0.twimg.com/profile_images/1842033988/422483_10150610296423674_540863673_9097491_251344996_n_reasonably_small.jpg"}]}
+
 ];
 
+window.vis = new Vis({el: $('#map')});
+yz.mapConfig.layers = [yz.baseLayer, yz.youzeers]
+vis.load(yz.mapConfig);
 
 window.onload = function() {
-    var vis = new Vis({el: $('#map')});
-    yz.mapConfig.layers = [yz.baseLayer, yz.youzeers]
-    vis.load(yz.mapConfig);
     vis.map.setCenter(yz.mapConfig.center);
     yz.vis = vis;
     yz.deployPeople();
     yz.bindHover();
+    setTimeout(function() {
+        $('h1 span').css('color', '#FFF')
+    },7000)
 }
 
 
